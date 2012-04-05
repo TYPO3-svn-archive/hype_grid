75dd8f6b8039c63f01eed94d79f6ca3701384887
thasmo <thasmo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-04-05 20:48:14 +0000

    Fixes #35654.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hype_grid/trunk@60428 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Classes/Utility/class.user_tx_hypegrid_content.php
@@ -0,0 +1,34 @@
+<?php
+
+class user_tx_hypegrid_content extends tx_cssstyledcontent_pi1 {
+
+   var $width;
+
+   public function __construct() {
+	  $this->width = max($GLOBALS['TSFE']->tmpl->setup['plugin.']['tx_hypegrid.']['grid.']['imageMinWidth'], 0);
+   }
+
+   public function render_textpic($content, $conf) {
+	  $this->cObj = $this->pObj->cObj;
+	  return parent::render_textpic($content, $conf);
+   }
+
+   protected function getImgColumnWidths($conf, $colCount, $netW) {
+
+	  $columnWidths = parent::getImgColumnWidths($conf, $colCount, $netW);
+
+	  if($this->width > 0) {
+		  foreach($columnWidths as $key => $width) {
+			$columnWidths[$key] = max($width, $this->width);
+		 }
+	  }
+
+	  return $columnWidths;
+   }
+
+   public function hookRequest($functionName) {
+	  return FALSE;
+   }
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -12,6 +12,9 @@ if(TYPO3_MODE == 'FE') {
 
 	# Page Renderer
 	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][$_EXTKEY] = 'EXT:' . $_EXTKEY . '/Classes/Hook/class.user_hypegrid_t3lib_pagerenderer.php:user_hypegrid_t3lib_pagerenderer->renderPreProcess';
+
+	# CSS Styled Content
+	$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['css_styled_content']['pi1_hooks']['render_textpic'] = t3lib_extMgm::extPath('hype_grid', 'Classes/Utility/class.user_tx_hypegrid_content.php:user_tx_hypegrid_content');
 }
 
 ?>
