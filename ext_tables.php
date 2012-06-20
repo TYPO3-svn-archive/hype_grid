12ec9a54ef42e07a6b3b814a1921d985d1064e95
thasmo <thasmo@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-06-20 09:48:02 +0000

    Fixes #37417.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/hype_grid/trunk@63601 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -39,7 +39,6 @@ $columns = array(
 			),
 		),
 	),
-
 	'tx_hypegrid_image_width' => array(
 		'exclude' => 1,
 		'label' => 'LLL:EXT:hype_grid/Resources/Private/Language/locallang_db.xml:tt_content.tx_hypegrid_image_width',
@@ -64,7 +63,6 @@ $columns = array(
 			),
 		),
 	),
-
 	'tx_hypegrid_image_margin' => array(
 		'exclude' => 1,
 		'label' => 'LLL:EXT:hype_grid/Resources/Private/Language/locallang_db.xml:tt_content.tx_hypegrid_image_margin',
@@ -89,7 +87,30 @@ $columns = array(
 			),
 		),
 	),
-
+	'tx_hypegrid_text_columns' => array(
+		'exclude' => 1,
+		'label' => 'LLL:EXT:hype_grid/Resources/Private/Language/locallang_db.xml:tt_content.tx_hypegrid_text_columns',
+		'config' => array(
+			'type' => 'select',
+			'size' => 1,
+			'maxitems' => 1,
+			'items' => array(
+				array('Default', 0),
+				array('1', 1),
+				array('2', 2),
+				array('3', 3),
+				array('4', 4),
+				array('5', 5),
+				array('6', 6),
+				array('7', 7),
+				array('8', 8),
+				array('9', 9),
+				array('10', 10),
+				array('11', 11),
+				array('12', 12),
+			),
+		),
+	),
 	'tx_hypegrid_hide_desktop' => array(
 		'exclude' => 1,
 		'label' => 'LLL:EXT:hype_grid/Resources/Private/Language/locallang_db.xml:tt_content.tx_hypegrid_hide_desktop',
@@ -130,6 +151,7 @@ $columns = array(
 
 t3lib_div::loadTCA('tt_content');
 t3lib_extMgm::addTCAcolumns('tt_content', $columns, 1);
+t3lib_extMgm::addToAllTCAtypes('tt_content', 'tx_hypegrid_text_columns', '', 'after:rte_enabled');
 t3lib_extMgm::addFieldsToPalette('tt_content', 'imageblock', '--linebreak--,tx_hypegrid_image_width,tx_hypegrid_image_margin');
 t3lib_extMgm::addFieldsToPalette('tt_content', 'visibility', '--linebreak--,tx_hypegrid_hide_desktop,tx_hypegrid_hide_tablet,tx_hypegrid_hide_phone');
 //t3lib_extMgm::addFieldsToPalette('tt_content', 'frames', 'imagecols,tx_hypegrid_element_width');
--- ext_tables.sql
+++ ext_tables.sql
@@ -5,6 +5,7 @@ CREATE TABLE tt_content (
 	tx_hypegrid_element_width tinyint(2) DEFAULT '0' NOT NULL,
 	tx_hypegrid_image_width tinyint(2) DEFAULT '0' NOT NULL,
 	tx_hypegrid_image_margin tinyint(2) DEFAULT '0' NOT NULL,
+	tx_hypegrid_text_columns tinyint(2) DEFAULT '0' NOT NULL,
 	tx_hypegrid_hide_desktop tinyint(1) DEFAULT '0' NOT NULL,
 	tx_hypegrid_hide_tablet tinyint(1) DEFAULT '0' NOT NULL,
 	tx_hypegrid_hide_phone tinyint(1) DEFAULT '0' NOT NULL
